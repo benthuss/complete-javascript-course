f2f39b92b850cdb7dcb1eb831ab8d4324a3b9e5f
[ben] <[ben@insightdesigns.com]>
2019-01-21 15:44:16 -0700

    files up to first code challenge

---
+++ 2-JS-basics/starter/scripts.js
@@ -0,0 +1,63 @@
+/**************************
+* Variables and data types
+**************************/
+
+// var firstName = 'Ben';
+// console.log(firstName);
+
+// var lastName = 'Thuss';
+// var age = 27;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+
+
+// job = 'Teacher';
+// console.log(job);
+
+// Variable mutation and type coercion
+
+// Type coercion
+// var firstName = 'Ben';
+// var age = 27;
+
+// console.log(firstName + ' ' + age);
+
+// var job, isMarried;
+// job = 'developer';
+// isMarried = false;
+
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+// age = 'twenty seven';
+// job = 'driver';
+
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName);
+
+/**************************
+* Basic Operators
+**************************/
+// var year, yearJohn, yearMark;
+// now = 2018;
+// yearJohn = now - 28;
+// yearMark = now - 33;
+
+// console.log(yearJohn);
+
+// Logical Operators
+
+
+// Operator Precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge);
